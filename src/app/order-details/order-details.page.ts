dae8afab95e4a4f6f3de68aee426400d5b8596db
jaro <jaro.helsen@hotmail.be>
2019-11-04 22:32:12 +0100

    implemented qr code saving

--- src/app/order-details/order-details.page.ts
+++ src/app/order-details/order-details.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Base64ToGallery } from '@ionic-native/base64-to-gallery/ngx';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-order-details',
@@ -7,11 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderDetailsPage implements OnInit {
 
-  qrcode="www.google.com"
+  qrData = 'www.google.com';
+  elementType: 'url' | 'canvas' | 'img' = 'canvas';
 
-  constructor() { }
+  constructor(private base64ToGallery: Base64ToGallery, private toastCtrl: ToastController) { }
 
   ngOnInit() {
+  }
+
+  downloadQR() {
+    const canvas = document.querySelector('canvas') as HTMLCanvasElement;
+    const imageData = canvas.toDataURL('image/jpeg').toString();
+    console.log(imageData);
+
+    let data = imageData.split(',')[1];
+    this.base64ToGallery.base64ToGallery(data, {
+      prefix: '_savedQR',
+      mediaScanner: true
+    }).then(async res => {
+      let toast = await this.toastCtrl.create({
+        header: 'QR code saved in library'
+      })
+    }, err => {
+      console.log('error', err);
+    });
   }
 
 }
