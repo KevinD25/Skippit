ec7caf78c80722c83b4bc4b25ed4a6cdaf21406b
jaro <jaro.helsen@hotmail.be>
2019-11-05 21:53:55 +0100

    fixed routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,20 @@ import { EstablishmentMenuItemPickerComponent } from './establishment-menu-item-
 import { MenuListPage } from './menu-list/menu-list.page';
 
 const routes: Routes = [
+<<<<<<< Updated upstream
   { path: '', loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule) },
   { path: 'detail-tab', loadChildren: './detail-tab/detail-tab.module#DetailTabPageModule' },
   { path: 'menu-list', loadChildren: './menu-list/menu-list.module#MenuListPageModule' },
   { path: 'check-order', loadChildren: './itemspicked/itemspicked.module#ItemspickedPageModule' },
   { path: 'order-details', loadChildren: './order-details/order-details.module#OrderDetailsPageModule' }
+=======
+  { path: '', loadChildren: './auth/auth.module#AuthPageModule' },
+  { path: '', loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule), canLoad: [AuthGuard]  },
+  { path: 'detail-tab', loadChildren: './detail-tab/detail-tab.module#DetailTabPageModule', canLoad: [AuthGuard]  },
+  { path: 'menu-list', loadChildren: './menu-list/menu-list.module#MenuListPageModule', canLoad: [AuthGuard]  },
+  { path: 'check-order', loadChildren: './itemspicked/itemspicked.module#ItemspickedPageModule', canLoad: [AuthGuard]  },
+  { path: '**', redirectTo:  '', pathMatch: 'full'}
+>>>>>>> Stashed changes
 ];
 @NgModule({
   imports: [
