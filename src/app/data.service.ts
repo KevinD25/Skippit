a20a10b1072bcaffb7d29d4c893d94d641fcd061
KOEN <spallierkoen@hotmail.com>
2019-11-05 02:42:59 +0100

    can see favorite places

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,21 +1,43 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'; 
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  Url = "https://localhost:44392/api/favorite"; 
+
+  favorite : Favorite = {PlaceId : 0}; 
 
   establishments:any[] = [
-    {Id: 1, Name: "Moonbucks", lat: "51.217050", lng: "4.420691", adress: "Koningin Astridplein 27, \n 2018 Antwerpen", ClosingTime: "20:00", OrderHour: "17:00"},
-    {Id: 2, Name: "Soubway"},
-    {Id: 3, Name: "BurgerQueen"},
-    {Id: 4, Name: "Babbel en Knabbel"},
-    {Id: 5, Name: "Panus"},
-    {Id: 6, Name: "LunchYard"}
+    {Id: 1, Name: "Moonbucks", lat: "51.217050", lng: "4.420691", adress: "Koningin Astridplein 27, \n 2018 Antwerpen", ClosingTime: "20:00", OrderHour: "17:00", Heart: "heart-empty"},
+    {Id: 2, Name: "Soubway", Heart: "heart-empty"},
+    {Id: 3, Name: "BurgerQueen", Heart: "heart-empty"},
+    {Id: 4, Name: "Babbel en Knabbel", Heart: "heart-empty"},
+    {Id: 5, Name: "Panus", Heart: "heart-empty"},
+    {Id: 6, Name: "LunchYard", Heart: "heart-empty"}
   ];
-  constructor() { }
+
+  favorites = new Subject<any>(); 
+
+  constructor(private http : HttpClient) { }
 
   getInfo(id): any{
     return this.establishments.find(establishment => establishment.Id == id);
   }
+  GetFavorites(){
+    this.http.get(this.Url).subscribe(x => {
+      this.favorites.next(x); 
+    })
+  }
+  AddFavorite(Id : number){
+    this.favorite.PlaceId = Id; 
+    this.http.post(this.Url, this.favorite).subscribe(x => this.favorites.next(x));  
+    console.log(this.favorite);
+    console.log(this.favorites); 
+  }
+}
+export interface Favorite{
+  PlaceId: number
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -10,13 +10,39 @@ import { DataService } from '../data.service';
 export class Tab2Page {
 
   establishments:any = [];
+  favorites:any = []; 
 
   constructor(public navCtrl: NavController, public data:DataService) {
     this.LoadData();
+    this.data.favorites.subscribe(x => {
+      this.favorites = x; 
+      this.SetHearts(); 
+    });
   }
 
   LoadData(){
     this.establishments = this.data.establishments;
+    this.data.GetFavorites();
+  }
+  SetHearts(){
+    this.establishments.forEach(establishment => {
+      this.favorites.forEach(favorite => {
+        if(establishment.Id == favorite.placeId){
+          this.establishments[establishment.Id-1].Heart = "heart"; 
+        }
+      });
+    });
+  }
+
+  Favorite(establishment){
+    if(establishment.Heart == "heart-empty"){
+      establishment.Heart = "heart"; 
+      this.data.AddFavorite(establishment.Id); 
+    }
+    else{
+      establishment.Heart = "heart-empty"; 
+      this.data.RemoveFavorite(establishment.Id); 
+    } 
   }
 
   ItemClicked(establishment){
