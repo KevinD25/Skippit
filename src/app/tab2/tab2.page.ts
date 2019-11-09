8ca68169872a19e54dbfe1a0c5c9507e91084530
jaro <jaro.helsen@hotmail.be>
2019-11-09 09:58:45 +0100

    Revert "unmerge master"
    This reverts commit 25db91cb4dd9626dd1caca5d92fc62f1552a08bb.

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular'
+import { DataService, Favorite } from '../services/data.service';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,64 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  establishments:any = [];
+  favorites:any = []; 
+  favorite : Favorite = {PlaceId : 0}; 
+  id : number = 0; 
 
+
+  constructor(public navCtrl: NavController, public data:DataService) {
+    this.LoadData();
+    this.GetFavorites(); 
+  }
+
+  LoadData(){
+    this.establishments = this.data.establishments;
+  }
+
+  GetFavorites(){
+    this.data.GetFavorites().subscribe(x => {
+      this.favorites = x; 
+      this.SetHearts(); 
+    });
+  }
+  
+  SetHearts(){
+    this.establishments.forEach(establishment => {
+      this.favorites.forEach(favorite => {
+        if(establishment.Id == favorite.placeId){
+          this.establishments[establishment.Id-1].Heart = "heart"; 
+        }
+      });
+    });
+  }
+
+  Favorite(establishment){
+    if(establishment.Heart == "heart-empty"){
+      establishment.Heart = "heart"; 
+      this.data.AddFavorite(establishment.Id).subscribe(x => this.GetFavorites()); 
+    }
+    else{
+      establishment.Heart = "heart-empty"; 
+      var x = 0;
+      var promise = new Promise((resolve, reject) => {
+        this.favorites.forEach(element => {
+          if(element.placeId == establishment.Id){
+            x = element.id; 
+          }
+        })
+        resolve(); 
+      });
+      promise.then(() => {        
+      this.data.RemoveFavorite(x).subscribe(x => {
+        console.log(x); 
+        this.GetFavorites()}); 
+        }); 
+    }     
+  }
+
+  ItemClicked(establishment){
+    console.log(establishment);
+    this.navCtrl.navigateForward('/tabs/tab2/info/' + establishment.Id);
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false, 
+  firebase: {
+    apiKey: "AIzaSyBbLuANgNrKs--w0i_zAsSIFXrQ4qIlRtk",
+    authDomain: "skippit-6314a.firebaseapp.com",
+    databaseURL: "https://skippit-6314a.firebaseio.com",
+    projectId: "skippit-6314a",
+    storageBucket: "skippit-6314a.appspot.com",
+    messagingSenderId: "877596530147",
+    appId: "1:877596530147:web:d7b1b00b0c8d1fcb237cd6",
+    measurementId: "G-KFCPB6WWLG"
+  }
 };
 
 /*
